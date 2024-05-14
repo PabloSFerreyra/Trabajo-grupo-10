451a76996145016984eff8da4a9018cf6a847ecf
David Ortiz <davideze.ortiz@gmail.com>
2024-05-14 11:13:35 -0300

    Guardado en el localStorage datos del formulario de contacto

--- js/main.js
+++ js/main.js
@@ -14,6 +14,8 @@ const subscribe = document.getElementById('subscribe');
 const submit = document.getElementById('submit');
 const error = document.getElementById('error');
 const form = document.getElementById('contactForm');
+let datosComprimidos = '';
+let datos = [];
 
 //Funcion de validacion de datos.
 submit.addEventListener('click', ()=>{
@@ -24,6 +26,23 @@ submit.addEventListener('click', ()=>{
     if(reason.value == '') return error.style.opacity = 100;
     if(find.value == '') return error.style.opacity = 100;
 
-    error.
+    guardarDatos(name.value, email.value, phone.value, message.value, reason.value, find.value);
     form.submit();
 });
+
+// Aca usamos el localstorage para almacenar los datos del usuario.
+
+function guardarDatos (name, email, phone, message, reason, find) {
+    datos.push({name: name, email: email, phone: phone, message: message, reason: reason, find: find})
+    
+    datosComprimidos = JSON.stringify(datos);
+    localStorage.setItem('datos', datosComprimidos);
+}
+
+// funcion para visualizar si estos datos se guardaron correctamente. (developer)
+
+function mostrar() {
+    datosComprimidos = localStorage.getItem('datos');
+    datos = JSON.parse(datosComprimidos);
+    console.log(datos)
+}
